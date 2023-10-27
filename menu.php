618c2854ee1efd801dfb1924f24e28c427c00dcd
PhantomStreetArtist <143738513+PhantomStreetArtist@users.noreply.github.com>
2023-10-26 17:44:45 -0700

    slight change on menu

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <?php
 echo '<a href="/index.php">Home</a> -
-<a href="/html/index.php">HTML </a> -
-<a href="/css/index.php">CSS </a> -
-<a href="/js/index.php">JavaScript </a> -
+<a href="html/index.php">HTML </a> -
+<a href="css/index.php">CSS </a> -
+<a href="js/index.php">JavaScript </a> -
 <a href="index.php">PHP </a>';
 ?>
