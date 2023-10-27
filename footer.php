1eec230486ab420e0ed5cb01ebbe4108371ca900
PhantomStreetArtist <143738513+PhantomStreetArtist@users.noreply.github.com>
2023-10-26 17:30:56 -0700

    adding files

---
+++ footer.php
@@ -0,0 +1,5 @@
+
+    <h1>By PHANTOM STREET ARTIST</h1>
+    <p><?php echo "Welcome to ART SAVES LIVES";?></p>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,4 @@
+
+<body>
+    <h1>QUESTIONING KULTURE IN QUESTION</h1>
+    <p><?php echo "ART SAVES LIVES";?></p>
